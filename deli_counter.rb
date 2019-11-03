63a7389b924ceef2196e5bf9c6d44db40ac5f147
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 05:59:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ def line(array)
   if array.empty?
     puts 'The line is currently empty.'
   else
-    puts 'arr here'
+    array.select.with_index { |val, key|
+      "#{"
+    }
   end
     
 end
