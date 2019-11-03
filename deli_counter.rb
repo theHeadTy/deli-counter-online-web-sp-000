58a494d2bba05d45f5aba5345140babdbf06b01d
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 06:04:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,13 @@ def line(array)
   if array.empty?
     puts 'The line is currently empty.'
   else
-    array.map.with_index { |val, key|
+    new = []
+    array.select.with_index { |val, key|
       key += 1
       #The line is currently: 1. Logan 2. Avi 3. Spencer
       "The line is currently: #{key}. #{val}"
     }
-    puts array
+    array
   end
     
 end
