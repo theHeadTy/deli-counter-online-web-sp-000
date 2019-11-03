aa40ceee8a8cd62d43452d865a0f177eb6ee1496
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 06:32:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+    puts 'The line is currently empty.'
+  else
+    new = []
+    array.each.with_index { |val, key|
+      key += 1
+      new << "#{key}. #{val}"
+    }
+    puts "The line is currently: #{new.join(" ")}"
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
+
+take_a_number([], 'Ada')
+take_a_number(["Logan", "Avi", "Spencer"], 'Grace')
+
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+line(katz_deli)
+line(other_deli)
+line(another_deli)
