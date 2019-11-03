4bcbf1083d39e91e50dd07f0510f67ce9dfceae3
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 05:52:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,8 @@
-class DeliCounter
-  attr_accessor :katz_deli, :other_deli, :another_deli
-  
+
   def line(array)
     puts 'The line is currently empty.'
   end
-  
-end
 
 deli = DeliCounter.new
 
-deli.line([])
+line([])
