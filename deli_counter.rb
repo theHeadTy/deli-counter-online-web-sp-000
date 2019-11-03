d801222197b972ad890c52b9b53b46274bf8dfa3
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 06:00:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(array)
     puts 'The line is currently empty.'
   else
     array.select.with_index { |val, key|
-      "#{"
+      #The line is currently: 1. Logan 2. Avi 3. Spencer
+      "The line is currently: #{key}. #{val}"
     }
   end
     
