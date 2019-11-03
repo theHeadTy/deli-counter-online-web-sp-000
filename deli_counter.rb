8f84f15fe62be9b940e97eb4098de7c1e0574c8e
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 06:00:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,8 @@ def line(array)
 end
 
 line([])
+
+
+  let(:katz_deli) { [] }
+  let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+  let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
