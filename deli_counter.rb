f4d904c03710884652edc943759c2b2103450d98
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 06:25:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,7 @@
 def line(array)
-  
   if array.empty?
     puts 'The line is currently empty.'
   else
-
     new = []
     array.each.with_index { |val, key|
       key += 1
@@ -11,8 +9,15 @@ def line(array)
     }
     puts "The line is currently: #{new.join(" ")}"
   end
-    
 end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+take_a_number([], 'Ada')
+take_a_number(["Logan", "Avi", "Spencer"], 'Grace')
 
 katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
