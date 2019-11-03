06ce667bd4a629018469d92d740e983e6f17861b
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 06:09:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(array)
     array.each.with_index { |val, key|
       key += 1
       #The line is currently: 1. Logan 2. Avi 3. Spencer
-      new << "The line is currently: #{key}. #{val}"
+      puts "The line is currently: #{key}. #{val}" next
     }
-    puts new
+    
   end
     
 end
